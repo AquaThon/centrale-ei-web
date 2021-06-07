6eac03eab41643a11f2ca3ce78ac5d74940808dd
mary <benjamin.mary@student-cs.fr>
2021-06-07 16:01:50 +0200

    first POC

---
+++ frontend/src/components/Movie.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class=movie-card>
+        <img :src="img" />
+        <h2>{{ movie.original_title}} : {{ movie.release_date}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        movie: Object,
+    },
+    data: function () {
+        return {
+            img: ''
+        }
+    },
+    created: function () {
+        console.log("https://image.tmdb.org/t/p/w500" + this.movie.poster_path);
+        this.img= "https://image.tmdb.org/t/p/w500" + this.movie.poster_path;
+        console.log(this.img)
+    }
+}
+</script>
+
+<style scoped>
+.movie-card {
+    text-align: center;
+}
+h2 {
+    width: 500px;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,78 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Welcome to Your Vue.js App</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          >eslint</a
-        >
+    <img alt="Vue logo" src="../assets/movie.png" />
+    <h1>! Films du jour !</h1>
+    <input v-model="movieName" placeholder="Movie Name" />
+    <p>Number of pages: {{ pages }}</p>
+    <p>Movie name is: {{ movieName }}</p>
+    <ul id="array-rendering">
+      <li v-for="(movie,index) in movies" :key=index>
+        <Movie v-if="movie.original_title.includes(movieName)" :movie="movie"/>
       </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank">vue-router</a>
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <button @click="morePages()"> Show More !</button>
   </div>
 </template>
 
 <script>
+
+import Movie from "@/components/Movie.vue";
+const axios = require('axios').default;
+
+
 export default {
+  data: function () {
+    return {
+      movieName: "",
+      movies: [],
+      page: 1
+    }
+  },
   name: "Home",
-};
+  created: function () {
+    this.fetchMovies(1)
+  },
+  methods: {
+    morePages: function () {
+      this.page = this.page + 1;
+      this.fetchMovies(this.page)
+    },
+    fetchMovies: function (page) {
+      axios
+        .get(`https://api.themoviedb.org/3/movie/popular?api_key=522d421671cf75c2cba341597d86403a&language=en-US&page=` + page)
+        .then((response) => {
+         console.log(response.data.results);
+         this.movies = this.movies.concat(response.data.results);
+       })
+       .catch((error) => {
+         console.log(error)
+      });
+    },
+  },
+  components: {
+    Movie,
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -97,5 +77,9 @@ li {
 
 a {
   color: #42b983;
+}
+
+#array-rendering {
+
 }
 </style>
