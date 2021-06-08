7a644ef38d18bff1fc73acc0fa0bcf6ed8e8a19c
mary <benjamin.mary@student-cs.fr>
2021-06-08 14:31:08 +0200

    added delete route

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -3,8 +3,19 @@ const MovieModel = require("../models/movie");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  console.log("succes!");
-  res.send([]);
+  MovieModel.find({}).then(function (movies) {
+    res.json({ movies: movies });
+  });
+});
+
+router.delete("/delete", function (req, res) {
+  MovieModel.deleteOne({ id: req.body.id }, function (err) {
+    if (err) res.status(500).json({ message: err });
+    else
+      res.status(201).json({
+        message: `Successfully deleted movie with id ${req.body.id} !`,
+      });
+  });
 });
 
 router.post("/new", function (req, res) {
@@ -23,7 +34,7 @@ router.post("/new", function (req, res) {
     .catch(function (error) {
       if (error.code === 11000) {
         res.status(400).json({
-          message: `User with id "${newMovie.id}" already exists`,
+          message: `Movie with id ${newMovie.id} already exists`,
         });
       } else {
         res.status(500).json({ message: "Error while creating the movie" });
