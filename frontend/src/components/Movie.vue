08a9c71ee8a6f66de92c07e2fcee7681e7e19f1e
Thon <thomas.borot@viarezo.fr>
2021-06-09 11:37:31 +0200

    Basis of new page

--- frontend/src/components/Movie.vue
+++ frontend/src/components/Movie.vue
@@ -9,7 +9,7 @@
       </div>
     </a>
     <div class="mainTitle">
-      <img :alt=this.moviePosterPath :src=this.moviePosterPath />
+      <img :alt="this.moviePosterPath" :src="this.moviePosterPath" />
       <h3 class="title">{{ movieOriginalTitle }}</h3>
     </div>
   </article>
@@ -28,40 +28,40 @@ export default {
 </script>
 
 <style>
-  .card {
-    flex: 0 0 250px;
-    margin: 10px;
-    border: 1px solid #ccc;
-    box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
-    background: #f0f0f0;
-    border-radius: 10px;
-    position: relative;
-    overflow: hidden;
-  }
-  .card img {
-    max-width: 100%;
-  }
-  .card .mainTitle {
-    max-width: 100%;
-    width: 100%;
-    height: 100%;
-  }
-  .card .mainTitle .title {
-    padding: 0 20px 20px;
-  }
-  .card .description {
-    text-align: justify;
-    padding-top: 10px;
-    padding-left: 10px;
-    padding-right: 10px;
-    height: 100%;
-    position: absolute;
-    color: rgba(0,0,0,0.0);
-    transition: 300ms ease;
-  }
-  .card .description:hover {
-    color: rgba(255, 255, 255, 1);
-    background: rgba(0, 0, 0, 0.8);
-    cursor: pointer;
-  }
+.card {
+  flex: 0 0 250px;
+  margin: 10px;
+  border: 1px solid #ccc;
+  box-shadow: 2px 2px 6px 0px rgba(0, 0, 0, 0.3);
+  background: #f0f0f0;
+  border-radius: 10px;
+  position: relative;
+  overflow: hidden;
+}
+.card img {
+  max-width: 100%;
+}
+.card .mainTitle {
+  max-width: 100%;
+  width: 100%;
+  height: 100%;
+}
+.card .mainTitle .title {
+  padding: 0 20px 20px;
+}
+.card .description {
+  text-align: justify;
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  height: 100%;
+  position: absolute;
+  color: rgba(0, 0, 0, 0);
+  transition: 300ms ease;
+}
+.card .description:hover {
+  color: rgba(255, 255, 255, 1);
+  background: rgba(0, 0, 0, 0.8);
+  cursor: pointer;
+}
 </style>
--- frontend/src/views/MovieShow.vue
+++ frontend/src/views/MovieShow.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="container">
-    <img :alt=this.moviePosterPath :src=this.moviePosterPath />
+  <p class="movieshow-title">{{ this.movieOriginalTitle }}</p>
+  <div class="movieshow-container">
+    <div class="movieshow-img" >
+      <img class="movieshow-inner-image" :alt="this.moviePosterPath" :src="this.moviePosterPath" />
+    </div>
+    <div class="movieshow-description">
+      {{ this.movieDescription }}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import axios from "axios";
+const posterPlaceHolderPath = require('@/assets/posterPlaceHolder.png')
 
 export default {
   name: "MovieShow",
@@ -20,24 +27,56 @@ export default {
   },
   methods: {
     fetchData: function () {
-      axios
-        .get(`${process.env.VUE_APP_BACKEND_BASE_URL}/movies/details/${this.movieId}`)
-        .then((response) => {
-          this.movieOriginalTitle = response.data.movieOriginalTitle;
-          this.moviePosterPath = response.data.moviePosterPath;
-          this.movieDescription = response.data.movieDescription;
-        })
-        .catch((error) => {
-          this.usersLoadingError = "An error occured while fetching movie details.";
-          console.error(error);
-        });
+      // axios
+      //   .get(
+      //     `${process.env.VUE_APP_BACKEND_BASE_URL}/movies/show/${this.movieId}`
+      //   )
+      //   .then((response) => {
+      //     this.movieOriginalTitle = response.data.movieOriginalTitle;
+      //     this.moviePosterPath = response.data.moviePosterPath;
+      //     this.movieDescription = response.data.movieDescription;
+      //   })
+      //   .catch((error) => {
+      //     this.usersLoadingError = "An error occured while fetching movie details.";
+      //     console.error(error);
+      //   });
+      this.movieOriginalTitle = "Voici le titre";
+      this.moviePosterPath = posterPlaceHolderPath;
+      this.movieDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.";
+
     },
   },
   created: function () {
     this.fetchData();
-  }
+  },
 };
 </script>
 
 <style>
+.movieshow-title {
+  padding: 10px 10px 10px 10px;
+  margin: 10px;
+  text-align: center;
+  font-family: "TeX Gyre Adventor";
+  font-size: xx-large;
+}
+.movieshow-container {
+  display: flex;
+  justify-content: stretch;
+  width: 100%;
+}
+.movieshow-img {
+  width: 40%;
+  height: 100%;
+  display: block;
+}
+.movieshow-img .movieshow-inner-image {
+  width: 100%;
+}
+.movieshow-description {
+  width: 60%;
+  padding: 10px 10px 10px 10px;
+  display: block;
+  align-content: center;
+}
 </style>
